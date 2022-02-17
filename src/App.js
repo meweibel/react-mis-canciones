9a69d5234f28934ac3854a1d2430e45b265c969d
DESKTOP-L5IIS42\eugew <eugenia.weibel@diveria.com>
2022-02-17 12:42:17 -0300

    MEW - Corrección por errores en el deploy
    Ajuste por errores en deploy

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   const [mySongs, setMySongs] = useState(mySongsInit);
   const [search, setSearch] = useState(searchInit);
   const [currentSong, setCurrentSong] = useState({});
-  const [error, setError] = useState(false);
+  const [setError] = useState(false);
 
   //Función de Efecto
   useEffect(() => {
